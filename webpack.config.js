9345c63fc85b147679cccac0d8ee1be36f4ae521
Romero Medeiros <romeromedeiros5@gmail.com>
2020-10-14 19:25:42 -0300

    Changes the 'output' folder

--- webpack.config.js
+++ webpack.config.js
@@ -3,5 +3,9 @@ const webpack = require('webpack')
 module.exports = {
   mode: 'development',
   // mode: 'production',
-  entry: './src/index.js'
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: __dirname + '/public'
+  }
 }
