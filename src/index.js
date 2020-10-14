c63fb7900578de35471fd3ebc2d39cb0b51cf649
Romero Medeiros <romeromedeiros5@gmail.com>
2020-10-14 19:40:03 -0300

    Adds and configures 'style-loader' and 'css-loader'

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import Person from './person'
+import './assets/css/main.css'
 
 const attendant = new Person()
 
--- webpack.config.js
+++ webpack.config.js
@@ -7,5 +7,14 @@ module.exports = {
   output: {
     filename: 'bundle.js',
     path: __dirname + '/public'
+  },
+  module: {
+    rules: [{
+      test: /\.css$/,
+      use: [
+        'style-loader', // adds the CSS into the DOM injecting the <style> tag
+        'css-loader', // interprets @import, ulr()...
+      ]
+    }]
   }
 }
