cea0eb47d184cc35cec3a9530fe1e8553d98be41
Romero Medeiros <romeromedeiros5@gmail.com>
2020-10-14 19:16:09 -0300

    Adds two samples using ES6 modules system

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import Person from './person'
+
+const attendant = new Person()
+
+console.log(attendant.cumpriment())
---
+++ src/person.js
@@ -0,0 +1,5 @@
+export default class Person {
+  cumpriment() {
+    return 'Hi!'
+  }
+}
